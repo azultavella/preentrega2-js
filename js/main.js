912ae0ec150d5a5986b5308ca7f760b7cc5283bd
azultavella <azultavella@gmail.com>
2023-05-30 12:43:26 -0300

    comprobacion del git push

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Clase Turno
+class Turno {
+    constructor(nombreApellido, telefono, correo, fecha) {
+        this.nombreApellido = nombreApellido;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.fecha = fecha;
+    }
+}
+
+// Array para almacenar los turnos
+const turnos = [];
+
+// Función para obtener un turno
+function obtenerTurno() {
+    while (true) {
+        const nombreApellido = prompt("Ingrese su nombre y apellido:");
+        const telefono = prompt("Ingrese su teléfono:");
+        const correo = prompt("Ingrese su correo electrónico:");
+        const fecha = prompt("Ingrese la fecha de turno (DD/MM/AAAA):");
+
+      // Función de orden superior para verificar si el turno ya fue asignado
+        const turnoYaAsignado = turnos.some((t) => t.fecha === fecha);
+    
+        if (turnoYaAsignado) {
+            alert("El turno para esa fecha ya fue asignado.");
+        } else {
+            const turno = new Turno(nombreApellido, telefono, correo, fecha);
+            turnos.push(turno);
+            alert("¡Turno asignado exitosamente!");
+        }
+    
+        const continuar = prompt("¿Desea obtener otro turno? (S/N)").toLowerCase();
+        if (continuar !== "s") {
+            break;
+        }
+    }
+}
+
+// Llamada a la función principal
+obtenerTurno();
